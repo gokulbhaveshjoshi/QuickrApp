1940a0064a895ae2eceb181f713d3557690c5e70
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-06-13 15:44:33 +0530

    Splash Screen

--- app/src/main/java/com/gokul/quickrapp/MainActivity.java
+++ app/src/main/java/com/gokul/quickrapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.gokul.quickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "SplashActivity", Toast.LENGTH_LONG).show();
+        splashScreen();
+    }
+
+    private void splashScreen() {
+        try{
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+//                /* Create an Intent that will start the Menu-Activity. */
+//                Intent mainIntent = new Intent(Splash.this,Menu.class);
+//                Splash.this.startActivity(mainIntent);
+//                Splash.this.finish();
+                toast();
+
+            }, 3000);
+        }
+
+        catch(Exception e){
+            Log.e("Splash Screen", ""+e);
+        }
+    }
+
+    private void toast() {
+        Toast.makeText(this, "SplashActivity", Toast.LENGTH_LONG).show();
     }
 }
